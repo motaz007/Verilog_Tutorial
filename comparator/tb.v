02656c3793e73f69e2b98c1e5880ef0b69fc0a9d
motaz007 <motaz.theab@gmail.com>
2020-10-27 15:47:35 +0100

    implementations of comparator and its testbench

---
+++ comparator/tb.v
@@ -0,0 +1,33 @@
+//defines the time scale of simulation, it will run in steps
+//of 1ns and has a precesion value of 1ps
+`timescale 1ns / 1ps
+module testbench;
+    //inputs
+    reg x;
+    reg y;
+    //output
+    wire z;
+    //instantiae the DUT
+    comparator uut(
+        .x(x),
+        .y(y),
+        .z(z)
+    );
+
+    initial begin
+        //initialize inputs
+    x = 0;
+    y = 0;
+
+    #10 x = 1;
+    #10 y = 1;
+    #20 x = 0;
+    #20 y = 1;
+    #40 ;
+    end
+
+        initial begin
+            $monitor("x=%d,y=%d,z=%d \n",x,y,z);
+        end
+
+endmodule
